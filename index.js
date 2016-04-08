1f6e21b8abbb3f3e2d1a636f88e9690c2dbf69ab
hingsir <hingsir1024@gmail.com>
2016-04-08 18:03:35 +0800

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var fileHelper = require('./libs/fileHelper.js')
 var _path_ = require('path')
 
 function deploy(dest){
-   //fileHelper.copyDir('./libs/aspnetMVC', dest);
+   fileHelper.copyDir('./libs/donet-route', dest);
    fileHelper.copyDir('./Views', dest + '/Views', function(viewPath, data){
      var viewbag = fs.readFileSync(_path_.dirname(viewPath) + '/'+ _path_.parse(viewPath).name + '.viewbag')
      return String(data).replace(/@\{/, function($){
